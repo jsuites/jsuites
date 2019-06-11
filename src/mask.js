73de23efa073e0eecd6bbccf4322973c23b3b41d
paulhodel <paul.hodel@gmail.com>
2019-06-11 13:10:34 +0100

    fix hh24

--- src/mask.js
+++ src/mask.js
@@ -348,7 +348,7 @@ jApp.mask = (function() {
                 } else if (mask[i] == 'd' && mask[i+1] == 'd') {
                     pieces.push('dd');
                     i++;
-                } else if (mask[i] == 'h' && mask[i+1] == 'h' && mask[i] == '2' && mask[i+1] == '4') {
+                } else if (mask[i] == 'h' && mask[i+1] == 'h' && mask[i+2] == '2' && mask[i+3] == '4') {
                     pieces.push('hh24');
                     i += 3;
                 } else if (mask[i] == 'h' && mask[i+1] == 'h') {
