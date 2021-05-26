8a0715000b44401667f77e54a6e9eed29d29a612
Guillaume Bonnaire <52194475+Guillaume-Bo@users.noreply.github.com>
2021-05-26 17:47:09 +0200

    New feature cascading management for modal

--- src/modal.js
+++ src/modal.js
@@ -28,6 +28,13 @@ jSuites.modal = (function(el, options) {
         obj.options.title = el.getAttribute('title');
     }
 
+    // Cascading Management
+    if(el.querySelector(".jmodal")!=null) {
+        var newEl =  el.cloneNode(false);
+        el.parentNode.appendChild(newEl);
+        return jSuites.modal(newEl, obj.options);
+    }
+
     var temp = document.createElement('div');
     while (el.children[0]) {
         temp.appendChild(el.children[0]);
@@ -83,6 +90,20 @@ jSuites.modal = (function(el, options) {
         } else {
             backdrop.style.display = 'block';
         }
+        // Cascading management
+        if (jSuites.modal.openedModalCascading.length > 0 && !obj.container.classList.contains("jmodal_fullscreen")) {
+            obj.container.style.top = "calc(50% + " + (5 * (jSuites.modal.openedModalCascading.length % 10))  + "px)";
+            obj.container.style.left = "calc(50% + " + (5 * (jSuites.modal.openedModalCascading.length % 10)) + "px)";
+
+            var backDrop = obj.container.parentNode.querySelector("div.jmodal_backdrop");
+            if (backDrop != null) {
+                backDrop.style.backgroundColor = "transparent";
+            }
+        }
+        // Push on cascading modal
+        if (jSuites.modal.current != null) {
+            jSuites.modal.openedModalCascading.push(jSuites.modal.current);
+        }
         // Current
         jSuites.modal.current = obj;
         // Event
@@ -108,6 +129,10 @@ jSuites.modal = (function(el, options) {
         jSuites.modal.current = null;
         // Remove fullscreen class
         obj.container.classList.remove('jmodal_fullscreen');
+        // Cascading managemenet
+        if (jSuites.modal.openedModalCascading.length > 0) {
+            jSuites.modal.current = jSuites.modal.openedModalCascading.pop();
+        }
         // Event
         if (typeof(obj.options.onclose) == 'function') {
             obj.options.onclose(el, obj);
@@ -157,6 +182,7 @@ jSuites.modal = (function(el, options) {
 
 jSuites.modal.current = null;
 jSuites.modal.position = null;
+jSuites.modal.openedModalCascading = [];
 
 jSuites.modal.keyDownControls = function(e) {
     if (e.which == 27) {
