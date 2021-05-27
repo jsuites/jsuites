be60408269ed97797fbd54bb94a2f202eba8795c
Guillaume Bonnaire <52194475+GBonnaire@users.noreply.github.com>
2021-05-27 17:38:52 +0200

    Add autofocus option
    Option for auto select first result on list after "find"

--- src/dropdown.js
+++ src/dropdown.js
@@ -169,6 +169,7 @@ jSuites.dropdown = (function(el, options) {
             onfocus: null,
             onblur: null,
             oninsert: null,
+            autofocus: false,
         }
 
         // Loop through our object
@@ -973,6 +974,11 @@ jSuites.dropdown = (function(el, options) {
             } else {
                 content.style.display = '';
             }
+        }
+        
+        // Auto focus
+        if(obj.options.autofocus==true) {
+            obj.firstVisible();
         }
     }
 
