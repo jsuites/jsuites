bc0caf35427918468d95a7a53cd5bb950d019787
= <paul.hodel@gmail.com>
2021-06-05 21:01:49 +0100

    v4.7.0

--- dist/jsuites.js
+++ dist/jsuites.js
@@ -17,7 +17,7 @@
 
 var jSuites = function(options) {
     var obj = {}
-    var version = '4.6.9';
+    var version = '4.7.0';
 
     var find = function(DOMElement, component) {
         if (DOMElement[component.type] && DOMElement[component.type] == component) {
