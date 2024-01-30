cef67a5171572794f26b9ff0f3a96d5755f510f4
Paul <you@example.com>
2024-01-30 16:38:28 +0000

    toFixed JS improvements

--- src/jsuites.js
+++ src/jsuites.js
@@ -55,7 +55,7 @@ var jSuites = {
     ...dictionary,
     ...helpers,
     /** Current version */
-    version: '5.1.0',
+    version: '5.1.1',
     /** Bind new extensions to Jsuites */
     setExtensions: function(o) {
         if (typeof(o) == 'object') {
