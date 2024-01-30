a75d6a6a37a0c4bc3fb565596ad8ad7a7f969fc2
nicolasjesse <nicolasjesseam@gmail.com>
2024-01-30 13:03:32 -0300

    add calendar tests

---
+++ tests/calendar.test.js
@@ -0,0 +1,29 @@
+const jSuites = require('../dist/jsuites');
+
+describe('Calendar', () => {
+    test('Calendar instance', () => {
+        document.body.innerHTML = '<div id="myDiv"></div>';
+        let div = document.getElementById('myDiv')
+        let calendar = jSuites.calendar(div, { value: '1999-09-20 00:00:00' })
+
+        expect(calendar.getValue()).toBe('1999-09-20 00:00:00')
+
+        calendar.setValue('2020-06-05 00:00:00')
+        expect(calendar.getValue()).toBe('2020-06-05 00:00:00')
+
+        calendar.next()
+        expect(calendar.getValue()).toBe('2020-07-05 00:00:00')
+
+        calendar.prev()
+        expect(calendar.getValue()).toBe('2020-06-05 00:00:00')
+
+        calendar.reset()
+        expect(calendar.getValue()).toBe('')
+    });
+
+    test('Calendar helpers', () => {
+        expect(jSuites.calendar.dateToNum('2023-03-15 00:00:00')).toBe(45000)
+        expect(jSuites.calendar.numToDate(45000)).toBe('2023-03-15 00:00:00')
+        expect(jSuites.calendar.extractDateFromString('20-09-1999', 'dd-mm-yyyy')).toBe('1999-09-20 00:00:00')
+    });
+});
