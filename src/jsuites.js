4a92791e257c763f204c9fa6765fd48cb73173ea
Paul <you@example.com>
2024-02-07 21:05:50 +0000

    version

--- src/jsuites.js
+++ src/jsuites.js
@@ -55,7 +55,7 @@ var jSuites = {
     ...dictionary,
     ...helpers,
     /** Current version */
-    version: '5.1.1',
+    version: '5.1.2',
     /** Bind new extensions to Jsuites */
     setExtensions: function(o) {
         if (typeof(o) == 'object') {
