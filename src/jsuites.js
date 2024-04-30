9a4f6bfe1e2d03d0d3f4e4c1a4d11db1d369ff85
Paul <you@example.com>
2024-04-30 18:20:52 +0100

    jupload box

--- src/jsuites.js
+++ src/jsuites.js
@@ -55,7 +55,7 @@ var jSuites = {
     ...dictionary,
     ...helpers,
     /** Current version */
-    version: '5.3.3',
+    version: '5.3.4',
     /** Bind new extensions to Jsuites */
     setExtensions: function(o) {
         if (typeof(o) == 'object') {
