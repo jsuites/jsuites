2bb7f6fb7e52459b07340a49bc25eb3707cfa482
Paul <you@example.com>
2024-09-11 13:55:32 +0100

    5.6.0 - mask to better handle scientific notation

--- tests/mask.test.js
+++ tests/mask.test.js
@@ -13,5 +13,8 @@ describe('jSuites mask', () => {
         expect(jSuites.mask.render(-1000000.0005, { mask: '0.000' }, true)).toBe('-1000000.001');
         expect(jSuites.mask.render(-11.449, { mask: '0.00' }, true)).toBe('-11.45');
         expect(jSuites.mask.render(-11.455, { mask: '0.00' }, true)).toBe('-11.46');
+        expect(jSuites.mask.render(79998007920000000000000, { mask: '#,##0' }, true)).toBe('79,998,007,920,000,000,000,000');
+        expect(jSuites.mask.render(79998007920000000000000, { mask: '0.0000E+00' }, true)).toBe('7.9998e+22');
+        expect(jSuites.mask.render(79998007920000000000000, { mask: '0.0000E+00' })).toBe('79998007920000000000000');
     });
 });
