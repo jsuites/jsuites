21073e888814634ca24a80f82c0ea488ea3864c5
Paul <you@example.com>
2025-04-03 17:09:07 +0100

    sw format

--- tests/mask.test.js
+++ tests/mask.test.js
@@ -4,6 +4,7 @@ describe('jSuites mask', () => {
 
     test('jSuites.mask.render', () => {
         expect(jSuites.mask.render(123, { mask: '00000'}, true)).toBe('00123');
+        expect(jSuites.mask.render(-13552.94219, { mask: '# ##0'}, true)).toBe('-13 553');
         expect(jSuites.mask.render(0.128899, { mask: '$ #.##0,00' }, true)).toBe('$ 0,13');
         expect(jSuites.mask.render(0.1, { mask: '0%' }, true)).toBe('10%');
         expect(jSuites.mask.render(1.005, { mask: '0.00' }, true)).toBe('1.01');
