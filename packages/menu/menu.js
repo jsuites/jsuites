302fe49ce241f658723c6ba6efe95069b792fc89
Paul <you@example.com>
2025-07-11 16:34:16 +0100

    menu 3.0.0

--- packages/menu/menu.js
+++ packages/menu/menu.js
@@ -24,30 +24,6 @@
         }
     }
 
-    const findElement = function(element, condition) {
-        let foundElement = false;
-
-        function path (element) {
-            if (element && ! foundElement) {
-                if (typeof(condition) == 'function') {
-                    foundElement = condition(element)
-                } else if (typeof(condition) == 'string') {
-                    if (element.classList && element.classList.contains(condition)) {
-                        foundElement = element;
-                    }
-                }
-            }
-
-            if (element.parentNode && ! foundElement) {
-                path(element.parentNode);
-            }
-        }
-
-        path(element);
-
-        return foundElement;
-    }
-
     const getWindowWidth = function() {
         let w = window;
         let d = document;
@@ -57,22 +33,34 @@
     }
 
     const selectOnLoad = function(adjustScroll) {
-        let m = document.querySelectorAll('.jmenu nav a');
+        let m = document.querySelectorAll('.jmenu li');
         for (let i = 0; i < m.length; i++) {
             m[i].classList.remove('selected');
         }
 
         let href = window.location.pathname;
         if (href) {
-            let menu = document.querySelector('.jmenu a[href="'+ href +'"]');
+            let menu = document.querySelector('.jmenu nav a[href="'+ href +'"]');
             if (menu) {
-                // Select header
-                const parent = menu.closest('nav');
-                if (parent) {
-                    parent.classList.add('selected');
+                const container = menu.closest('li');
+                if (container) {
+                    container.classList.add('selected');
                 }
-                // Select child
-                menu.classList.add('selected');
+
+                // Select header
+                let item = container;
+                while (item && item.parentNode) {
+                    if (item.tagName !== 'NAV') {
+                        item = item.parentNode;
+
+                        if (item.previousElementSibling?.classList.contains('folder')) {
+                            item.previousElementSibling.classList.add('selected');
+                        }
+                    } else {
+                        item = false;
+                    }
+                }
+
                 // Direct scroll to element
                 if (adjustScroll === true) {
                     // Force a small delay to ensure DOM is ready
@@ -112,85 +100,21 @@
 
         obj.load = function() {
             if (options.adjustOnLoad !== false) {
-                if (window.localStorage) {
-                    let menu = el.querySelectorAll('nav');
-                    for (let i = 0; i < menu.length; i++) {
-                        if (menu[i].getAttribute('data-id')) {
-                            let state = window.localStorage.getItem('jmenu-' + menu[i].getAttribute('data-id'));
-                            if (state === '1') {
-                                menu[i].classList.add('selected');
-                            } else {
-                                if (state === '0' || !menu[i].classList.contains('selected')) {
-                                    menu[i].classList.remove('selected');
-                                }
-                            }
-                        }
-                    }
-                }
-
                 selectOnLoad(true);
-
-                let a = el.querySelectorAll('a');
-                for (let i = 0; i < a.length; i++) {
-                    if (a[i].nextElementSibling && a[i].nextElementSibling.tagName === 'UL') {
-                        a[i].classList.add('jmenu-subitem');
-                        a[i].addEventListener('mousedown', function() {
-                            if (a[i].classList.contains('opened')) {
-                                a[i].classList.remove('opened');
-                            } else {
-                                a[i].classList.add('opened');
-                            }
-                        })
-                    }
-                }
             }
         }
 
-        obj.update = function() {
-            selectOnLoad();
+        obj.update = function(adjustScroll) {
+            selectOnLoad(adjustScroll);
         }
 
         obj.select = function(o, e) {
-            if (o.tagName === 'NAV') {
-                let m = el.querySelectorAll('nav');
-                for (let i = 0; i < m.length; i++) {
-                    m[i].style.display = 'none';
-                }
-                o.style.display = '';
-                o.classList.add('selected');
-            } else {
-                let m = el.querySelectorAll('nav a');
-                for (let i = 0; i < m.length; i++) {
-                    m[i].classList.remove('selected');
-                }
-                o.classList.add('selected');
 
-                // Better navigation
-                if (options && options.collapse === true) {
-                    if (o.classList.contains('show')) {
-                        m = el.querySelectorAll('nav');
-                        for (let i = 0; i < m.length; i++) {
-                            m[i].style.display = '';
-                        }
-                        o.style.display = 'none';
-                    } else {
-                        m = el.querySelectorAll('nav');
-                        for (let i = 0; i < m.length; i++) {
-                            m[i].style.display = 'none';
-                        }
-
-                        m = el.querySelector('.show');
-                        if (m) {
-                            m.style.display = 'block';
-                        }
-
-                        m = findElement(o.parentNode, 'selected');
-                        if (m) {
-                            m.style.display = '';
-                        }
-                    }
-                }
+            let m = el.querySelectorAll('li.selected');
+            for (let i = 0; i < m.length; i++) {
+                m[i].classList.remove('selected');
             }
+            o.parentNode.classList.add('selected');
 
             if (options && typeof(options.onclick) == 'function') {
                 options.onclick(obj, e);
@@ -203,16 +127,14 @@
         }
 
         let action = function(e) {
-            if (e.target.tagName === 'H2') {
-                if (e.target.parentNode.classList.contains('selected')) {
-                    e.target.parentNode.classList.remove('selected');
-                    localStorage.setItem('jmenu-' + e.target.parentNode.getAttribute('data-id'), 0);
+            if (e.target.classList.contains('folder')) {
+                if (e.target.classList.contains('selected')) {
+                    e.target.classList.remove('selected');
                 } else {
-                    e.target.parentNode.classList.add('selected');
-                    localStorage.setItem('jmenu-' + e.target.parentNode.getAttribute('data-id'), 1);
+                    e.target.classList.add('selected');
                 }
             } else if (e.target.tagName === 'A') {
-                // Mark link as selected
+                // Mark the link as selected
                 obj.select(e.target, e);
             } else {
                 // Get target info
@@ -251,9 +173,6 @@
 
         return obj;
     });
-
-    // Events
-    window.addEventListener('popstate', selectOnLoad);
 
     if (window.jSuites) {
         jSuites.setExtensions({ menu: Plugin });
