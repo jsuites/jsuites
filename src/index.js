6d7a77e26f5e3a211af8a3e3d8447943e71bc5a2
Paul <you@example.com>
2025-09-05 19:24:43 +0100

    CSP review

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import jSuites from './jsuites';
-import heatmap from '../packages/heatmap/heatmap';
-import '../packages/heatmap/heatmap.css';
+import cropper from '../packages/cropper/cropper';
+import '../packages/cropper/cropper.css';
 
 import '../dist/jsuites.css';
 
-var initialDate = '2021-01-01';
 
-var year = [];
-var date = new Date(initialDate);
-
-while (year.length < 366) {
-    year.push({
-        date: date.toISOString().slice(0, 10),
-        value: Math.random() * 10,
-    });
-
-    date.setDate(date.getDate() + 1);
-}
-
-heatmap(document.getElementById('root'), {
-    title: 'Example',
-    data: year,
-    date: initialDate,
-    colors: ['#B2DFDB', '#4DB6AC', '#009688', '#00796B', '#004D40'],
-    tooltip: true,
-});
+cropper(document.getElementById('root'), {
+    area: [ 280, 280 ],
+    crop: [ 150, 150 ],
+})
