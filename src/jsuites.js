54fabbe4ce4b34376b28cd33a8b0c51da61c948e
Paul <you@example.com>
2025-10-07 10:51:22 +0100

    fix autoCasting for currency

--- src/jsuites.js
+++ src/jsuites.js
@@ -58,7 +58,7 @@ var jSuites = {
     ...dictionary,
     ...helpers,
     /** Current version */
-    version: '6.0.0-beta.5',
+    version: '6.0.0-beta.6',
     /** Bind new extensions to Jsuites */
     setExtensions: function(o) {
         if (typeof(o) == 'object') {
