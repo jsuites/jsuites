fd4d0aaf909abd1c962d1e63b62de1748c6749e0
Paul <you@example.com>
2026-01-07 12:49:13 +0000

    contextmenu fix on salesforce.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,44 @@ import jSuites from './jsuites';
 
 import '../dist/jsuites.css';
 
-//jSuites.tags(document.getElementById('root'));
+
+let contextMenu = jSuites.contextmenu(root, {
+    items: [
+        {
+            title: 'About',
+            shortcut: 'Ctrl + A',
+            onclick: function () {
+                alert('About is clicked');
+            },
+        },
+        {
+            title: 'Go to Jspreadsheet Pro website',
+            onclick: function () {
+                window.open('https://jspreadsheet.com/v7');
+            },
+        },
+        {
+            type: 'line'
+        },
+        {
+            title: 'Save',
+            icon: 'save'
+        },
+        {
+            title: 'Submenus',
+            shortcut: '►',
+            submenu: [
+                {
+                    title: 'Submenu 1',
+                    shortcut: 'Ctrl + X',
+                }
+            ]
+        },
+    ],
+})
+
+
+document.getElementById('container').addEventListener("contextmenu", function(e) {
+    contextMenu.open(e);
+    e.preventDefault();
+});
