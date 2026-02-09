cbbcab63cb9ccc6536f92f61f2d60a1864567f97
Paul <you@example.com>
2026-02-09 13:09:07 +0000

    tests

--- src/index.js
+++ src/index.js
@@ -2,44 +2,7 @@ import jSuites from './jsuites';
 
 import '../dist/jsuites.css';
 
+window.j = jSuites;
 
-let contextMenu = jSuites.contextmenu(root, {
-    items: [
-        {
-            title: 'About',
-            shortcut: 'Ctrl + A',
-            onclick: function () {
-                alert('About is clicked');
-            },
-        },
-        {
-            title: 'Go to Jspreadsheet Pro website',
-            onclick: function () {
-                window.open('https://jspreadsheet.com/v7');
-            },
-        },
-        {
-            type: 'line'
-        },
-        {
-            title: 'Save',
-            icon: 'save'
-        },
-        {
-            title: 'Submenus',
-            shortcut: '►',
-            submenu: [
-                {
-                    title: 'Submenu 1',
-                    shortcut: 'Ctrl + X',
-                }
-            ]
-        },
-    ],
-})
+// jSuites.tags(root);
 
-
-document.getElementById('container').addEventListener("contextmenu", function(e) {
-    contextMenu.open(e);
-    e.preventDefault();
-});
